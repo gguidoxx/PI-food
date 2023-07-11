66dd27c09932d407248ac89d597c16def9997e13
gguidoxx <gudombe@gmail.com>
2023-07-10 23:55:35 -0300

    cambios env

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,18 @@ import RecipeCreate from "./components/RecipeCreate";
 import Detail from "./components/Detail";
 import AfterCreate from "./components/AfterCreate";
 import Error from "./components/Error404/404";
-import { useId } from "react";
 
 function App() {
-  const randomID = useId();
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/home" component={Home} key={randomID} />
+          <Route exact path="/home" component={Home} key={Math.random()}/>
           <Route exact path="/recipes" component={RecipeCreate} />
           <Route exact path="/recipes/:id" component={Detail} />
           <Route exact path="/after-created" component={AfterCreate} />
-          <Route path="/*" component={Error} />
+          <Route path="/*" component={Error}/>
         </Switch>
       </div>
     </BrowserRouter>
